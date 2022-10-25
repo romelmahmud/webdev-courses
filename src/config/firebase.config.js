810e9440d99aa59531ce3c82d6a9619570ac1414
Romel Mahmud <romelmahmud.dev@gmail.com>
2022-10-26 05:31:59 +0600

    firebase setup complete with github

---
+++ src/config/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCl80qvL2wIndC5CQBTHOMJumsDumu_TzQ",
+  authDomain: "webdev-courses.firebaseapp.com",
+  projectId: "webdev-courses",
+  storageBucket: "webdev-courses.appspot.com",
+  messagingSenderId: "656148265526",
+  appId: "1:656148265526:web:4668a9d43cec386f57f697",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
