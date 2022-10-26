0128cda87dadd3b2403f086646d048bc2169fc47
Romel Mahmud <romelmahmud.dev@gmail.com>
2022-10-27 04:45:24 +0600

    some design change on sidebar

--- src/pages/AllCourses/Sidebar.jsx
+++ src/pages/AllCourses/Sidebar.jsx
@@ -4,14 +4,16 @@ const Sidebar = ({ categories }) => {
   console.log(categories);
   return (
     <aside className=" md:w-1/5 rounded-lg shadow-md transition hover:shadow-xl bg-white dark:bg-gray-800 text-gray-900 dark:text-white p-4">
-      <h3 className="text-2xl text-center">Courses Categories</h3>
+      <h3 className="text-2xl text-center text-blue-600 dark:text-blue-500  font-medium">
+        Courses Categories
+      </h3>
       <ul className="mt-6">
         {categories?.map((category) => (
           <li
-            className="py-4 px-6 hover:bg-gray-100 cursor-pointer hover:dark:bg-gray-700 "
+            className=" my-4 hover:bg-blue-50 cursor-pointer hover:dark:bg-gray-700 hover:text-blue-700 hover:rounded-md"
             key={category.id}
           >
-            <button className="font-semibold text-lg dark:text-gray-300 text-gray-800">
+            <button className="py-3 px-5 font-semibold text-lg dark:text-gray-300 text-gray-800 hover:text-blue-700 hover:dark:text-blue-500 rounded">
               {category.name}
             </button>
           </li>
